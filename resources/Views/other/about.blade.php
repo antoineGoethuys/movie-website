5dc0f706cf4c884c6c4551e84072f73ee88b9861
DragonRouge513 <antoine.goethuys@student.ehb.be>
2024-01-15 16:55:13 +0100

    Add sources used to about page

--- resources/Views/other/about.blade.php
+++ resources/Views/other/about.blade.php
@@ -10,4 +10,16 @@
             <p class="card-text">We believe in innovation and are constantly striving to improve our services to meet the changing needs of our clients.</p>
         </div>
     </div>
+    <div class="container mt-5 card">
+        <h2 class="card-title">Sources Used</h2>
+        <div class="card-body">
+            <ul>
+                <li><a href="https://grafikart.fr/tutoriels/introduction-laravel-2112">Introduction à Laravel - Grafikart</a></li>
+                <li>GitHub Copilot</li>
+                <li>ChatGPT</li>
+                <li><a href="https://laravel.com/docs/10.x">Documentation officielle de Laravel 10</a></li>
+                <li><a href="https://getbootstrap.com/docs/">Documentation officielle de Bootstrap</a></li>
+            </ul>
+        </div>
+    </div>
 @endsection
